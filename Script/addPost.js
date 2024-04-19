110ed40bbec918e786033cd0f5f0f83cb329a43d
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2024-04-19 22:42:37 +0530

    some changes

--- Script/addPost.js
+++ Script/addPost.js
@@ -5,8 +5,11 @@ export default function postCard(img, title, para, id) {
     <div class="post-img">
     <img class="card-img" src="${img}" id='${id}' alt="img" onerror="this.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiqNdH4rC5njlxoA0Iwru70QCslWJItVBH2Q&usqp=CAU'"></div>
               <div class="post-heading">
-                <a href="/article.html#${id}"><h2 class="card-heading">${title}</h2></a>
-                <p class="card-para">${para}</p>
+                <a href="/article.html#${id}"><h2 class="card-heading">${title.slice(
+    0,
+    45
+  )}...</h2></a>
+                <p class="card-para">${para.slice(0, 80)}...</p>
               </div>
                 <button class="main-read-later" id=${id}>Read Later</button>
     `;
