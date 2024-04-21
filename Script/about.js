f787c377e65d76d030fbef5ee123efafc7a9fd3a
Abdur Rab Khan <146613551+abdurrabkhan3263@users.noreply.github.com>
2024-04-21 21:24:24 +0530

    Delete Script/about.js

--- Script/about.js
+++
@@ -1,5 +0,0 @@
-let btn = document.querySelector(".about-me-btn");
-
-btn.addEventListener("click", function () {
-  window.location.href = `https://www.linkedin.com/in/abdur-rab-khan-188a672a7`;
-});
